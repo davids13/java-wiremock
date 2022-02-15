3ec7277e1b5c2f2ddc5a650bd861c94c6cd2c5e5
David Aniceto <David.Aniceto@ctw.bmwgroup.com>
2022-02-15 23:15:47 +0000

    This commit was done by amend git cmd

---
+++ src/main/java/com/github/davids13/boundary/MockResource.java
@@ -0,0 +1,9 @@
+package com.github.davids13.boundary;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MockResource {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+}
